830f7498447d8970af52bc7fe0019d37644638c2
AxleCode <93900125+AxleCode@users.noreply.github.com>
2022-08-30 08:11:01 +0700

    Add files via upload
    Fungsi Login Checkout

---
+++ cek_login.php
@@ -0,0 +1,87 @@
+<?php 
+// mengaktifkan session pada php
+session_start();
+ 
+// menghubungkan php dengan koneksi database
+include 'db.php';
+ 
+// menangkap data yang dikirim dari form login
+$username = $_POST['username'];
+$password = $_POST['password'];
+ 
+ 
+// menyeleksi data user dengan username dan password yang sesuai
+$login = mysqli_query($koneksi,"select * from admin where username = '".$username."' AND password = '".md5($password)."' ");
+// menghitung jumlah data yang ditemukan
+$cek = mysqli_num_rows($login);
+ 
+// cek apakah username dan password di temukan pada database
+if($cek > 0){
+ 
+	$data = mysqli_fetch_assoc($login);
+ 
+	// cek jika user login sebagai admin
+	if($data['id_admin']=="1"){
+ 
+		// buat session login dan super admin
+		$_SESSION['username'] = $username;
+		$_SESSION['id_admin'] = "1";
+		// alihkan ke halaman super admin
+		header("location:superadmin.php");
+ 
+	// cek jika user login sebagai sekretariat
+	}else if($data['id_admin']=="2"){
+		// buat session login dan username
+		$_SESSION['username'] = $username;
+		$_SESSION['id_admin'] = "2";
+		// alihkan ke halaman sekretariat 
+		header("location:sekretariatadmin.php");
+ 
+	// cek jika user login sebagai bidang tata lingkungan admin
+	}else if($data['id_admin']=="10"){
+		// buat session login dan username
+		$_SESSION['username'] = $username;
+		$_SESSION['id_admin'] = "10";
+		// alihkan ke halaman bidang tata lingkungan admin
+		header("location:btladmin.php");
+ 
+
+    }else if($data['id_admin']=="4"){
+		// buat session login dan username bidang penaatan
+		$_SESSION['username'] = $username;
+		$_SESSION['id_admin'] = "4";
+		// alihkan ke halaman dashboard bidang penaatan
+		header("location:penaatanadmin.php");
+ 
+    }else if($data['id_admin']=="5"){
+		// buat session login dan username bidang pengendalian
+		$_SESSION['username'] = $username;
+		$_SESSION['id_admin'] = "5";
+		// alihkan ke halaman dashboard bidang pengendalian
+		header("location:pengendalianadmin.php");
+ 
+    }else if($data['id_admin']=="6"){
+		// buat session login dan username bidang pengelolaan sampah
+		$_SESSION['username'] = $username;
+		$_SESSION['id_admin'] = "6";
+		// alihkan ke halaman dashboard bidang pengelolaan sampah
+		header("location:persampahanadmin.php");
+ 
+    }else if($data['id_admin']=="7"){
+		// buat session login dan username uptd lab admin
+		$_SESSION['username'] = $username;
+		$_SESSION['id_admin'] = "7";
+		// alihkan ke halaman dashboard uptd lab admin
+		header("location:labadmin.php");
+ 
+	}else{
+ 
+		// alihkan ke halaman login kembali
+        header("location:login.php?pesan=gagal");
+        
+	}	
+}else{
+    header("location:login.php?pesan=gagal");
+}
+ 
+?>
---
+++ login.php
@@ -0,0 +1,86 @@
+<?php 
+                    if(isset($_GET['pesan'])){
+                      if($_GET['pesan']=="gagal"){
+                        echo "<script>alert('Username dan Password tidak sesuai !');</script>";
+                      }
+                    }
+                  ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+  <meta http-equiv="x-ua-compatible" content="ie=edge" />
+  <title>Login Admin Database - DLH Kab Mojokerto</title>
+  <!-- MDB icon -->
+  <link rel="icon" href="img/protection.png" />
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.2/css/all.css" />
+  <!-- Google Fonts Roboto -->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700;900&display=swap" />
+  <!-- MDB -->
+  <link rel="stylesheet" href="css/bootstrap-login-form.min.css" />
+</head>
+
+<body>
+  <!-- Start your project here-->
+  <section class="vh-100" style="background-color: rgb(0, 23, 43);">
+    <div class="container py-5 h-100">
+      <div class="row d-flex justify-content-center align-items-center h-100">
+        <div class="col col-xl-10">
+          <div class="card" style="border-radius: 1rem;">
+            <div class="row g-0">
+              <div class="col-md-6 col-lg-5 d-none d-md-block">
+                <img
+                  src="img/p.jpg"
+                  alt="login form"
+                  class="img-fluid" style="border-radius: 1rem 0 0 1rem;"
+                />
+              </div>
+              <div class="col-md-6 col-lg-7 d-flex align-items-center">
+                <div class="card-body p-4 p-lg-5 text-black">
+  
+                  <form action="cek_login.php" method="post">
+  
+                    <div class="d-flex align-items-center mb-3 pb-1">
+                      <img src="img/logo.png" alt="" width="350px"><i  style="color: #ff6219;"></i>
+                    </div>
+  
+                    <h5 class="fw-normal mb-3 pb-3" style="letter-spacing: 1px;">Login to a Specific Admin Account</h5>
+  
+                    <div class="form-outline mb-4">
+                      <input type="text" name="username" class="form-control form-control-lg" required/>
+                      <label class="form-label" for="form2Example17">Username</label>
+                    </div>
+  
+                    <div class="form-outline mb-4">
+                      <input type="password" name="password" class="form-control form-control-lg" required/>
+                      <label class="form-label" for="form2Example27">Password</label>
+                    </div>
+  
+                    <div class="pt-1 mb-4">
+                      <input type="submit" name="submitadmin" value="Login" class="btn btn-dark btn-lg btn-block">
+                    </div>
+                    
+                    <a href="#!" class="small text-muted">&copy; <a href="">2022 IT Team DLH Kab Mojokerto</a>, All Right Reserved.</a>
+                  </form>
+                
+
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- End your project here-->
+
+  <!-- MDB -->
+  <script type="text/javascript" src="js/mdb.min.js"></script>
+  <!-- Custom scripts -->
+  <script type="text/javascript"></script>
+</body>
+
+</html>
